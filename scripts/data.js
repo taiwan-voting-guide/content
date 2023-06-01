ef33db20135b4fc2af90a4e753ac1c5f754a606f
gnituy18 <you@hsuyuting.com>
2023-06-01 23:42:52 +0800

    Fix script

--- scripts/data.js
+++ scripts/data.js
@@ -1,35 +1,28 @@
+const path = require("path");
+const { writeFile } = require("node:fs/promises");
+
 const projectId = process.env.MIXPANEL_PROJECT_ID;
 const bookmarkId = process.env.MIXPANEL_BOOKMARK_ID;
 const secret = process.env.MIXPANEL_SECRET;
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    authorization: `Basic ${secret}`,
-  },
-};
-
 (async function () {
-  try {
-    const response = await fetch(
-      `https://mixpanel.com/api/2.0/insights?project_id=${projectId}&bookmark_id=${bookmarkId}`,
-      options
-    );
-  } catch (error) {
-    console.error(error);
+  const response = await fetch(
+    `https://mixpanel.com/api/2.0/insights?project_id=${projectId}&bookmark_id=${bookmarkId}`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        authorization: `Basic ${secret}`,
+      },
+    }
+  );
+
+  const mixpanelData = await response.json();
+  if (mixpanelData.error) {
+    console.error(mixpanelData.error);
     return;
   }
-  const mixpanelData = await response.json();
-  const tags = Object.keys(mixpanelData.series["Tag - Total"])
-    .filter((key) => key !== "$overall")
-    .map((key) => {
-      return {
-        key,
-        value: mixpanelData.series["Tag - Total"][key].all,
-      };
-    })
-    .sort((a, b) => b.value - a.value);
+
   const data = {
     title: "tag_clicks_last_7_days",
     name: "過去7天標籤點擊數",
@@ -38,10 +31,25 @@ const options = {
     from_data: mixpanelData.date_range.from_date,
     to_data: mixpanelData.date_range.to_date,
     data: {
-      total: mixpanelData.series["Tag - Total"]["$overall"].all,
-      tags,
+      total: 0,
+      tags: [],
     },
   };
 
-  console.log(JSON.stringify(data));
+  const total = mixpanelData.series["Tag - Total"];
+  if (total) {
+    data.data.total = total["$overall"].all;
+    data.data.tags = Object.keys(mixpanelData.series["Tag - Total"])
+      .filter((key) => key !== "$overall")
+      .map((key) => {
+        return {
+          key,
+          value: total[key].all,
+        };
+      })
+      .sort((a, b) => b.value - a.value);
+  }
+
+  const filePath = path.join(__dirname, `../content/data/${data.title}.json`);
+  await writeFile(filePath, JSON.stringify(data));
 })();
