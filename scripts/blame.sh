11e2a890ba5304e02e68c6de6769defe86142801
許煜廷 <you@hsuyuting.com>
2023-11-12 01:47:33 +0800

    feat: add blames

---
+++ scripts/blame.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "No files provided. Exiting."
+  exit 1
+fi
+
+for politician in $@ ; do
+  politician=${politician#content/politician}
+  politician=${politician%.md}
+  git blame -e -c "content/politician/$politician.md" > "content/blame/$politician.txt"
+done
